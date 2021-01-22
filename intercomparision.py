f61565c4953cf4a0854598d7b784e3df0748988e
Eventeicher <elizabeth.venteicher@valpo.edu>
2021-01-22 14:48:45 -0600

    perhaps with dist calc?

--- intercomparision.py
+++ intercomparision.py
@@ -56,10 +56,11 @@ import config as plot_config
 import copy
 
 from read_pforms import read_TInsitu, pform_names
+def nearest(items, pivot):
+    return min(items, key=lambda x: abs(x - pivot))
 
 pd.set_option('display.max_columns', None)
-df_holder =[]
-p_holder=[]
+df_holder, p_holder =[], [] 
 for p in pform_names('TInsitu'):
     print(p)
     print('............')
@@ -69,15 +70,69 @@ for p in pform_names('TInsitu'):
         df['pname'] = p
         df = df.drop(columns=['U','V', 'dir', 'spd' ])
         if ptype =='NSSL':
-            df = df.drop(columns=['id', 'qc1', 'qc2', 'qc3', 'qc4'])
+            df = df.drop(columns=['id', 'qc1', 'qc2', 'qc3', 'qc4', 'all_qc_flags'])
         df_holder.append(df)
         p_holder.append(p)
-        #  print(df)
 full_df= pd.concat(df_holder)
 new_cols=['dist_Prb1', 'dist_Prb2', 'dist_FFld', 'dist_Wins', 'dist_LiDR', 'dist_CoMeT1', 'dist_CoMeT2']
 for c in new_cols:
     full_df.insert(2, c, np.nan)
 full_df.set_index(['pname', 'datetime'], inplace=True)
-print(full_df)
+#  print(full_df.loc['FFld','datetime'])
 
+test_dict= {}
+for pform in p_holder:
+    p_df = full_df.loc[pform].index
+    for p_time in p_df:
+        #actual timeboundary
+        time_offset_outer= timedelta(minutes=5)
+        #  start_t_outer, end_t_outer = (p_time-time_offset_outer).to_pydatetime(), (p_time + time_offset_outer).to_pydatetime()
+        start_t_outer, end_t_outer = (p_time-time_offset_outer), (p_time + time_offset_outer)
+        #most data points will fall within the time bound this tighter bound just decreases the #of calcs needed
+        time_offset_inner= timedelta(minutes=1)
+        #  start_t_inner, end_t_inner = (p_time-time_offset_inner).to_pydatetime(), (p_time + time_offset_inner).to_pydatetime()
+        start_t_inner, end_t_inner = (p_time-time_offset_inner), (p_time + time_offset_inner)
 
+        for comp_pform in p_holder:
+            #subset data to a smaller time range 
+            comp_df = full_df.loc[comp_pform].loc[start_t_inner:end_t_inner]
+            if len(comp_df) == 0: 
+                #  test if there is data within the actual time window we want (might just need to include more calcs)
+                comp_df = full_df.loc[comp_pform].loc[start_t_outer:end_t_outer]
+            
+            #if there still is not data in the range pass otherwise add to the dataframe
+            if len(comp_df) != 0: 
+                comp_p_time = nearest(comp_df.index, p_time)
+                if comp_pform == 'FFld': comp_col= 'diff_FFld'
+                elif comp_pform == 'WinS': comp_col= 'diff_Wins'
+                elif comp_pform == 'LIDR': comp_col= 'diff_LiDR'
+                elif comp_pform == 'Prb1': comp_col= 'diff_Prb1'
+                elif comp_pform == 'Prb2': comp_col= 'diff_Prb2'
+                elif comp_pform == 'CoMeT1': comp_col= 'diff_CoMeT1'
+                elif comp_pform == 'CoMeT2': comp_col= 'diff_CoMeT2'
+                elif comp_pform == 'CoMeT3': comp_col= 'diff_CoMeT3'
+
+                dist = np.square(comp_df['lat']-full_df.loc[(pform, p_time),'lat'])+ np.square(comp_df['lon']-full_df.loc[(pform, p_time),'lon'])
+                full_df[(pform,p_time), comp_col] = dist
+                #  test_dict.update(comp_pform=comp_p_time)
+            #  else:
+                #  test_dict.update(comp_pform= np.nan)
+    #  print(test_dict)
+    print('hey')
+    print(full_df)
+        #  for comp_pform in p_holder:
+            #  print(p_df)
+        #  comp_p_time = nearest(comps.loc[comp_pform].index, p_time)
+    #  for p_time in p_df:
+        #  for comp_pform in p_holder:
+            #  print(p_time)
+            #  print(comps)
+            #  comp_p_time = nearest(comps.loc[comp_pform].index, p_time)
+            #  comp_p_time = nearest(full_df.loc[comp_pform].index, p_time)
+            #  300 sec = 5min
+            #  if abs(comp_p_time - p_time).seconds <= 300:
+                #  test_dict.update({comp_pform: comp_p_time})
+        #  print(test_dict)
+                #  print("p: {}, c_p: {}, p_time: {}, comp_p_time: {}".format(pform, comp_pform, p_time, comp_p_time))
+#  print(test.iloc[3])
+#  print(near)
